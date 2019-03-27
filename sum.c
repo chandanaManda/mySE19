aaf2083b98fe76ac4af50bbe2a19b4955b1ec565
cmanda <chandanamanda68@gmail.com>
2019-03-27 10:18:16 -0500

    This is my secong version v#

--- sum.c
+++ sum.c
@@ -3,12 +3,15 @@
 #include<stdio.h>
 int main()
 {
-  int i,num,sum=0; //initialising the values 
+  int i,num,sum=0,count; //initialising the values
+  float average;
   printf("please enter the value of number :\n"); //enter the number 
   scanf("%d",&num);//read the value
   printf(" the value of number is :%d \n",num);
   for(i= 1;i <=num;i++)
-    sum +=i; //writing the logic  
+    sum +=i; //writing the logic
+  count = i;
   printf("the sum of number from 1 to num = %d is sum = %d \n",num,sum);
+  printf(" the average of numbers is : %f \n" ,sum/count);
   return 0;
 }//end of main 
